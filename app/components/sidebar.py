13ba925c62b538eaf510a9c7faddb42d3c1497c9
Pablo Marin <pabmar@microsoft.com>
2023-03-13 05:43:13 +0000

    improved sidebar

--- app/components/sidebar.py
+++ app/components/sidebar.py
@@ -8,9 +8,12 @@ def sidebar():
 
         st.markdown("---")
         st.markdown("# About")
-        st.markdown(
-            "GPT Smart Search allows you to ask questions about your "
-            "documents and get accurate answers with instant citations. "
-            "This engine finds information from ~9700 Computer Science Publications in Arxiv from 2020-2022"
+        st.markdown("""
+            GPT Smart Search allows you to ask questions about your
+            documents and get accurate answers with instant citations.
+            
+            This engine finds information from ~9.8k [Computer Science Publications in Arxiv from 2020-2022](https://www.kaggle.com/datasets/1b6883fb66c5e7f67c697c2547022cc04c9ee98c3742f9a4d6c671b4f4eda591)
+            
+            [Github Repo](https://github.com/pablomarin/GPT-Azure-Search-Engine/)
         )
         st.markdown("---")
