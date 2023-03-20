f6acddef9cb1fc4415be7629a29742751a2546db
Pablo Marin <pablomarin@users.noreply.github.com>
2023-03-20 12:39:43 -0500

    Update credentials.py

--- app/credentials.py
+++ app/credentials.py
@@ -3,9 +3,16 @@
 API_VERSION = '2021-04-30-Preview'
 DATASOURCE_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=demodatasetsp;AccountKey=K9rLf3hFmKN4oZunp3xIIqjlxkyqtOfcdJQgDk65cHSYAY29rvoTx2S1x2t+mJ5tv+3LRN/FES3Q+AStuqGYIg==;EndpointSuffix=core.windows.net"
 
-AZURE_SEARCH_ENDPOINT = "Enter your Azure Cognitive Search Endpoint ..."
-AZURE_SEARCH_KEY = "Enter your Azure Cognitive Search Primary Key ..."
-COG_SERVICES_NAME = "Enter your Azure Cognitive services multi-service account name ..."
-COG_SERVICES_KEY = "Enter your Azure Cognitive services multi-service Key ..."
-AZURE_OPENAI_ENDPOINT = "Enter your Azure OpenAI Endpoint ..."
-AZURE_OPENAI_KEY = "Enter your Azure OpenAI Key ..."
+# AZURE_SEARCH_ENDPOINT = "Enter your Azure Cognitive Search Endpoint ..."
+# AZURE_SEARCH_KEY = "Enter your Azure Cognitive Search Primary Key ..."
+# COG_SERVICES_NAME = "Enter your Azure Cognitive services multi-service account name ..."
+# COG_SERVICES_KEY = "Enter your Azure Cognitive services multi-service Key ..."
+# AZURE_OPENAI_ENDPOINT = "Enter your Azure OpenAI Endpoint ..."
+# AZURE_OPENAI_KEY = "Enter your Azure OpenAI Key ..."
+
+AZURE_SEARCH_ENDPOINT = "https://azure-cog-search-bbuqqkw6y2eee.search.windows.net"
+AZURE_SEARCH_KEY = "jg0tHBuOz26mF9Res0c2fvGzd1cDOAMHXCallFY5rzAzSeBphgw5"
+COG_SERVICES_NAME = "cognitive-service-bbuqqkw6y2eee"
+COG_SERVICES_KEY = "327ef6b7219c40fdac14fbffd548a803"
+AZURE_OPENAI_ENDPOINT = "https://open-ai-pinternal.openai.azure.com/"
+AZURE_OPENAI_KEY = "6848da60869d4b319161ebf6ea61e643"
