d1382336611ee21f3c9ceb4ebfa567b7b8603ad6
Pablo Marin <pabmar@microsoft.com>
2023-03-30 03:41:42 +0000

    small changes

--- app/main.py
+++ app/main.py
@@ -144,7 +144,7 @@ if qbutton or bbutton or st.session_state.get("submit"):
                 st.markdown("#### Answer")
                 st.markdown(answer["output_text"].split("SOURCES:")[0])
                 st.markdown("Sources:")
-                st.markdown(answer["output_text"].split("SOURCES:")[1].replace(" ","").split(","))
+                st.markdown(print (s+"<br>") for s in answer["output_text"].split("SOURCES:")[1].replace(" ","").split(","))
                 st.markdown("---")
                 st.markdown("#### Search Results")
 
