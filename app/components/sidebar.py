2417ed2ad5c16f89a002d6b61f076856d2ea3ef4
Pablo Marin <pabmar@microsoft.com>
2023-04-06 04:15:34 +0000

    Changed in-memory English model to all-MiniLM-L12-v2

--- app/components/sidebar.py
+++ app/components/sidebar.py
@@ -14,7 +14,7 @@ def sidebar():
             
             This engine finds information from the following:
             - ~10k [Computer Science Publications in Arxiv from 2020-2022](https://www.kaggle.com/datasets/1b6883fb66c5e7f67c697c2547022cc04c9ee98c3742f9a4d6c671b4f4eda591)
-            - ~34k [COVID-19 literature in LitCovid from 2020-2023](https://www.ncbi.nlm.nih.gov/research/coronavirus/docsum)
+            - ~52k [COVID-19 literature in LitCovid from 2020-2023](https://www.ncbi.nlm.nih.gov/research/coronavirus/)
             
             [Github Repo](https://github.com/pablomarin/GPT-Azure-Search-Engine/)
         """
