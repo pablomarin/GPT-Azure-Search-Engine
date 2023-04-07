157a47bd6bad304629b75a829b7aeaf0bda2fe09
giorgiosaez <gsaezd@outlook.com>
2023-04-07 22:27:59 +0000

    add sample file for tabular

--- app/pages/2_Tabular_Data_(Preview).py
+++ app/pages/2_Tabular_Data_(Preview).py
@@ -1,6 +1,11 @@
 import streamlit as st
-import time
-import numpy as np
+import os
+import pandas as pd
+from langchain.llms import AzureOpenAI
+from langchain.chat_models import AzureChatOpenAI
+from langchain.agents import create_pandas_dataframe_agent
+from langchain.agents import create_csv_agent
+
 
 def sidebar():
     with st.sidebar:
@@ -17,14 +22,6 @@ def sidebar():
         st.session_state["AZURE_OPENAI_ENDPOINT"] = st.text_input("Enter your Azure OpenAI Endpoint:")
         st.session_state["AZURE_OPENAI_API_KEY"] = st.text_input("Enter Azure OpenAI Key:", type="password")
                 
-import streamlit as st
-import os
-import pandas as pd
-from langchain.llms import AzureOpenAI
-from langchain.chat_models import AzureChatOpenAI
-from langchain.agents import create_pandas_dataframe_agent
-from langchain.agents import create_csv_agent
-
 preffix = 'First set the pandas display options to show all the columns, then get the column names, then answer the question: '
 suffix = '. ALWAYS before giving the Final Answer, reflect on the answer and ask yourself if it answers correctly the original question. If you are not sure, try another method. \n If the two runs does not give the same result, reflect again two more times until you have two runs that have the same result. If you still cannot arrive to a consistent result, say that you are not sure of the answer. But, if you are sure of the correct answer, create a beautiful and thorough response. ALWAYS, as part of your final answer, explain how you got to the answer. Format the final answer in Markdown language'
 
@@ -37,8 +34,18 @@ sidebar()
 
 def clear_submit():
     st.session_state["submit"] = False
+    
+    
+col1, col2 = st.columns([1,1])
+with col1:
+    uploaded_file  = st.file_uploader(label = "Upload your tabular CSV file", type="csv", accept_multiple_files=False, key=None, help="Upload your CSV file that contains tabular data, make sure that the first row corresponds to the columns", on_change=None, disabled=False)
+with col2:
+    st.markdown("Or pick from these sample datasets:")
+    st.markdown("[Covid Tracking Project](https://learn.microsoft.com/en-us/azure/open-datasets/dataset-covid-tracking?tabs=azure-storage) ")
+    ingest_button = st.button("Load Sample CSV") # Give button a variable name
 
-uploaded_file  = st.file_uploader(label = "Upload your tabular CSV file", type="csv", accept_multiple_files=False, key=None, help="Upload your CSV file that contains tabular data, make sure that the first row corresponds to the columns", on_change=None, disabled=False)
+if ingest_button: # Make button a condition.
+    uploaded_file = "https://pandemicdatalake.blob.core.windows.net/public/curated/covid-19/covid_tracking/latest/covid_tracking.csv"
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
@@ -49,7 +56,7 @@ if uploaded_file is not None:
     qbutton = st.button('Generate Answer')
 
 
-    if qbutton or st.session_state.get("submit"):
+    if (qbutton or st.session_state.get("submit")) and uploaded_file:
         if not query_str:
             st.error("Please enter a question")
         else:
