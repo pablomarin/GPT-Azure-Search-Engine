50a79621939efd4378822fd0915aa7f5455310c0
Pablo Marin <pabmar@microsoft.com>
2023-04-08 09:42:46 +0000

    fix Home page

--- app/Home.py
+++ app/Home.py
@@ -1,40 +1,6 @@
 import streamlit as st
-import urllib
-import os
-import time
-import requests
-import random
-from collections import OrderedDict
-from openai.error import OpenAIError
-from langchain.docstore.document import Document
-
-from components.sidebar import sidebar
-from utils import (
-    embed_docs,
-    get_answer,
-    get_sources,
-    search_docs
-)
-from credentials import (
-    DATASOURCE_CONNECTION_STRING,
-    AZURE_SEARCH_API_VERSION,
-    AZURE_SEARCH_ENDPOINT,
-    AZURE_SEARCH_KEY,
-    COG_SERVICES_NAME,
-    COG_SERVICES_KEY,
-    AZURE_OPENAI_ENDPOINT,
-    AZURE_OPENAI_KEY,
-    AZURE_OPENAI_API_VERSION
-
-)
-
-os.environ["OPENAI_API_BASE"] = os.environ["AZURE_OPENAI_ENDPOINT"] = st.session_state["AZURE_OPENAI_ENDPOINT "] = AZURE_OPENAI_ENDPOINT
-os.environ["OPENAI_API_KEY"] = os.environ["AZURE_OPENAI_API_KEY"] = st.session_state["AZURE_OPENAI_API_KEY"] = AZURE_OPENAI_KEY
-os.environ["OPENAI_API_VERSION"] = os.environ["AZURE_OPENAI_API_VERSION"] = AZURE_OPENAI_API_VERSION
 
 st.set_page_config(page_title="GPT Smart Search", page_icon="📖", layout="wide")
-
-
 
 st.image("https://user-images.githubusercontent.com/113465005/226238596-cc76039e-67c2-46b6-b0bb-35d037ae66e1.png")
 
