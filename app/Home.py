53d2c272b951edc61deae96302c4a8a51cd57bbc
Pablo Marin <pabmar@microsoft.com>
2023-04-12 23:46:51 +0000

    Fix Notebook 05

--- app/Home.py
+++ app/Home.py
@@ -13,9 +13,9 @@ st.markdown("""
     documents and get accurate answers with instant citations.
     
     This engine finds information from the following:
-    - ~10k [Computer Science Publications in Arxiv from 2020-2022](https://www.kaggle.com/datasets/1b6883fb66c5e7f67c697c2547022cc04c9ee98c3742f9a4d6c671b4f4eda591)
-    - ~52k [COVID-19 literature in LitCovid from 2020-2023](https://www.ncbi.nlm.nih.gov/research/coronavirus/)
-    - [Covid Tracking Project Dataset](https://learn.microsoft.com/en-us/azure/open-datasets/dataset-covid-tracking?tabs=azure-storage). A CSV with up-to-date tabular information of Covid cases in the US.
+    - ~10k [Computer Science Publications in Arxiv from 2020-2021](https://www.kaggle.com/datasets/1b6883fb66c5e7f67c697c2547022cc04c9ee98c3742f9a4d6c671b4f4eda591)
+    - ~52k [COVID-19 literature in LitCovid from 2020-2021](https://www.ncbi.nlm.nih.gov/research/coronavirus/)
+    - [Covid Tracking Project Dataset](https://covidtracking.com/). A CSV with tabular information of Covid cases and hospitalizations in the US from 2020-2021.
     
     **👈 Select a demo from the sidebar** to see some examples
     of what Azure Cognitive Search and Azure OpenAI Service can do!
---
+++ download_odbc_driver.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if ! [[ "18.04 20.04 22.04" == *"$(lsb_release -rs)"* ]];
+then
+    echo "Ubuntu $(lsb_release -rs) is not currently supported.";
+    exit;
+fi
+
+echo "Downloading..."
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
+echo "Updating apt-get.."
+apt-get update
+
+echo "Installing msodbcsql18.."
+ACCEPT_EULA=Y apt-get install -y msodbcsql18
+# optional: for bcp and sqlcmd
+ACCEPT_EULA=Y apt-get install -y mssql-tools18
+echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
+source ~/.bashrc
+# optional: for unixODBC development headers
+apt-get install -y unixodbc-dev
+echo "DONE"
