e4e92ec687e92692f42fb6e1fe8c3733eeb4e46c
Pablo Marin <pabmar@microsoft.com>
2023-04-29 08:11:23 +0000

    Improve Notebook 5 and 6

--- app/prompts.py
+++ app/prompts.py
@@ -202,3 +202,54 @@ DETECT_LANGUAGE_PROMPT = PromptTemplate(
     template=detect_language_template,
 )
 
+
+_mssql_prompt = """
+You are an MS SQL expert. Given an input question, first create a syntactically correct MS SQL query to run, then look at the results of the query and return the answer to the input question.
+
+Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the TOP clause as per MS SQL. You can order the results to return the most informative data in the database.
+
+Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in square brackets ([]) to denote them as delimited identifiers.
+
+Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
+
+## **Do not use double quotes on the SQL query**. 
+
+** ALWAYS before giving the Final Answer, try another method**. Then reflect on the answers of the two methods you did and ask yourself if it answers correctly the original question. If you are not sure, try another method.
+If the runs does not give the same result, reflect and try again two more times until you have two runs that have the same result. If you still cannot arrive to a consistent result, say that you are not sure of the answer. But, if you are sure of the correct answer, create a beautiful and thorough response. DO NOT MAKE UP AN ANSWER OR USE PRIOR KNOWLEDGE, ONLY USE THE RESULTS OF THE CALCULATIONS YOU HAVE DONE. 
+
+ALWAYS, as part of your final answer, explain how you got to the answer on a section that starts with: \n\nExplanation:\n.'
+
+Use the following format:
+
+Question: "Question here"
+SQLQuery: "SQL Query to run"
+SQLResult: "Result of the SQLQuery"
+Answer: "Final answer here"
+Explanation:
+
+Only use the following tables:
+{table_info}
+
+Question: {input}"""
+
+MSSQL_PROMPT = PromptTemplate(
+    input_variables=["input", "table_info", "top_k"], template=_mssql_prompt
+)
+
+CSV_PROMPT_PREFIX = """
+First set the pandas display options to show all the columns, get the column names, then answer the question.
+
+"""
+
+CSV_PROMPT_SUFFIX = """
+
+## **NEVER** use ` or ``  as on any of your responses.
+## As part of your process, **NEVER** display or print a full dataframe
+
+**ALWAYS** before giving the Final Answer, try another method. Then reflect on the answers of the two methods you did and ask yourself if it answers correctly the original question. If you are not sure, try another method.
+
+If the runs does not give the same result, reflect and try again two more times until you have two runs that have the same result. If you still cannot arrive to a consistent result, say that you are not sure of the answer. But, if you are sure of the correct answer, create a beautiful and thorough response. **DO NOT MAKE UP AN ANSWER OR USE PRIOR KNOWLEDGE, ONLY USE THE RESULTS OF THE CALCULATIONS YOU HAVE DONE**. 
+
+## **ALWAYS**, as part of your "Final Answer", explain how you got to the answer on a section that starts with: "\n\nExplanation:\n". In the explanation, mention the column names that you used to get to the final answer. 
+.
+"""
