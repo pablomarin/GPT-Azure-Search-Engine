d404d02f1183cae6d7d703ec8b6be271409ad7ad
Frode Hitland <frode.hitland@nov.com>
2024-01-26 15:03:44 +0000

    all nb runs

--- download_odbc_driver.sh
+++ download_odbc_driver.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-if ! [[ "18.04 20.04 22.04" == *"$(lsb_release -rs)"* ]];
+# Update this list to include the versions of Ubuntu you want to support
+SUPPORTED_VERSIONS="18.04 20.04 22.04 24.04"
+
+if ! [[ "$SUPPORTED_VERSIONS" == *"$(lsb_release -rs)"* ]];
 then
     echo "Ubuntu $(lsb_release -rs) is not currently supported.";
     exit;
@@ -10,15 +13,17 @@ echo "Downloading..."
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
-echo "Updating apt-get.."
+echo "Updating apt-get..."
 apt-get update
 
-echo "Installing msodbcsql18.."
-ACCEPT_EULA=Y apt-get install -y msodbcsql17
+# Update the package names as per the available versions
+echo "Installing msodbcsql..."
+ACCEPT_EULA=Y apt-get install -y msodbcsql18 # Update this to the correct version
 # optional: for bcp and sqlcmd
-ACCEPT_EULA=Y apt-get install -y mssql-tools
-echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
+ACCEPT_EULA=Y apt-get install -y mssql-tools # Ensure the version matches the installed msodbcsql
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc # Update PATH if the version of mssql-tools has changed
 source ~/.bashrc
 # optional: for unixODBC development headers
 apt-get install -y unixodbc-dev
+
 echo "DONE"
